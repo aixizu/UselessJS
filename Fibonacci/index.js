5024b9d53b3326e092b1a23847aedee6f70acf2f
aixizu <67578661+aixizu@users.noreply.github.com>
2022-11-07 04:31:49 +0000

    💫 - Added squaring cuz memories <3
    if (fibonacci) { cry(); )

--- Fibonacci/index.js
+++ Fibonacci/index.js
@@ -16,13 +16,51 @@ function fibonacci(n)
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+function fibonacciSquare(n) 
+{
+    return fibonacci(n) * fibonacci(n);
+}
+
+function Square()
+{
+    reader.question('Enter a number: ', (number) => 
+    {
+        console.log('The square of the fibonacci number is: ', fibonacciSquare(number));
+    });
+}
+
 function Sequence()
 {
     reader.question('Enter a number: ', (number) => 
     {
         console.log('The fibonacci sequence is: ', fibonacci(number));
-        Menu();
     });
 }
 
-Sequence();
+function Menu()
+{
+    console.log('1. Fibonacci Sequence');
+    console.log('2. Fibonacci Square');
+    console.log('3. Exit');
+    reader.question('Enter a number: ', (number) => 
+    {
+        switch (number)
+        {
+            case '1':
+                Sequence();
+                break;
+            case '2':
+                Square();
+                break;
+            case '3':
+                reader.close();
+                break;
+            default:
+                console.log('Invalid input');
+                Menu();
+                break;
+        }
+    });
+}
+
+Menu();
