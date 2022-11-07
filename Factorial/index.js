5dab7cab1b348107eea83d7ead157dfd0078ab92
aixizu <67578661+aixizu@users.noreply.github.com>
2022-11-07 04:33:43 +0000

    💌 - Factorial solver

---
+++ Factorial/index.js
@@ -0,0 +1,27 @@
+const input = require('readline');
+
+const reader = input.createInterface
+({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function factorial(n)
+{
+    if (n < 2)
+    {
+        return n;
+    }
+
+    return n * factorial(n - 1);
+}
+
+function Read()
+{
+    reader.question('Enter a number: ', (number) =>
+    {
+        console.log('The factorial is: ', factorial(number));
+    });
+}
+
+Read();
